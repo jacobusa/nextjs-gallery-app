d5fa45e026b07fffa389f33e4d61e263a9513ce3
Kobus <jacobusbad@gmail.com>
2024-06-15 12:16:19 -0700

    make home dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Link from "next/link";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
 const mockUrls = [
   `https://utfs.io/f/6bc77d50-d933-408f-9ded-edef0e8fd5a6-na1y5.png`,
   `https://utfs.io/f/e925b09e-477c-4e1e-9572-6c42b3777399-c124hi.png`,

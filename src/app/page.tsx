220dedb295f4481cc1ff2d1350201ebb0d343a5d
Kobus <jacobusbad@gmail.com>
2024-06-15 12:06:30 -0700

    Conifigure database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   `https://utfs.io/f/6bc77d50-d933-408f-9ded-edef0e8fd5a6-na1y5.png`,
@@ -11,17 +12,20 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
   return (
     <main>
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + index} className="w-48">
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} alt="image.id" />
           </div>
         ))}
       </div>
-      hey
     </main>
   );
 }
